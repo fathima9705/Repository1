6c2ff4335a9e94852e78534a759c9ba16e977826
noor fathima <fathima.noor9705@gmail.com>
2026-03-05 04:04:46 +0000

    added fact.c

---
+++ fact.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+// Function to calculate factorial iteratively
+unsigned long long factorial(int n) {
+    unsigned long long fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a non-negative integer: ");
+    // Input validation
+    if (scanf("%d", &num) != 1) {
+        printf("Invalid input. Please enter an integer.\n");
+        return 1;
+    }
+
+    if (num < 0) {
+        printf("Error: Factorial is not defined for negative numbers.\n");
+        return 1;
+    }
+
+    // Factorial grows very fast; warn if number is too large
+    if (num > 20) {
+        printf("Warning: Factorial of %d may overflow 64-bit storage.\n", num);
+    }
+
+    unsigned long long result = factorial(num);
+    printf("Factorial of %d = %llu\n", num, result);
+
+    return 0;
+}
